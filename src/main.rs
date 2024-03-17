1f3a75e51147e5b2bd6779b97e020d5ad22d465b
Jonas Peitz <jonaspeitz@proton.me>
2024-03-17 15:29:27 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,58 @@
 mod parser;
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    compile: Option<String>,
+    #[arg(short, long)]
+    keep: Option<String>,
+    #[arg(short, long)]
+    output: Option<String>,
+    #[arg(short, long)]
+    lower_true: Option<String>,
+    #[arg(long)]
+    input: Option<Vec<String>>,
+    #[arg(long)]
+    args: Option<Vec<String>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let mut path_prefix = String::new();
+    let mut path_suffix = String::new();
+    let mut include = String::new();
+    let mut include_prefix = String::new();
+    let mut parse_que: Vec<String> = Vec::new();
+
+    match &args.compile {
+        Some(x) => {
+            path_prefix = "".to_string();
+        }
+        None => {
+            path_prefix = "python_".to_string();
+        }
+    }
+    match &args.keep {
+        Some(x) => {
+            path_prefix = "".to_string();
+        }
+        None => {
+            path_prefix = "python_".to_string();
+        }
+    }
+    match args.input {
+        Some(x) => parse_que = x,
+        None => parse_que = Vec::new(),
+    }
+    if let Some(x) = &args.compile {
+        if let Some(y) = args.args {
+            for arg in y {
+                parse_que.push(arg);
+            }
+        }
+    }
+
+    for _i in usize::try_from(parse_que.len()) {}
 }
